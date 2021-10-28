e40d6cebdd87e1efcfe3e295bc7609af8acc9ba0
Victoria282 <vilkova_viktoria2000@mail.ru>
2021-10-28 20:09:22 +0500

    Добавлен адаптер с item_layout Выводится заголовок Не отображается картинка

--- app/src/main/java/com/example/victoria_intersvyaz_test/MainActivity.kt
+++ app/src/main/java/com/example/victoria_intersvyaz_test/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.victoria_intersvyaz_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.victoria_intersvyaz_test.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.dataFragment))
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
